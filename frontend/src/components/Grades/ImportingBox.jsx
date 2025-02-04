ed2b9ad561b48d7830539b8b37f9a83023d3f409
Younes <pryyounes@gmail.com>
2025-02-04 20:59:57 +0100

    tmp

---
+++ frontend/src/components/Grades/ImportingBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ImportingBox() {
+	return (
+		<div className="w-11/12 h-24 m-auto mb-5 rounded-lg bg-amber-400 shadow-lg shadow-amber-400 hover:scale-105 duration-500">
+			<div className="w-full h-full flex justify-center items-center">
+				<button className="text-2xl font-bold text-gray-800">
+					Importer les notes
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default ImportingBox; 
